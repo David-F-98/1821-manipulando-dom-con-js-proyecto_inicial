1f2836fd96f5c9f56f178dca9e38e603441c7e27
David Nieto <u2903149@unimilitar.edu.co>
2023-03-28 18:38:41 -0500

    JavaScript-DOM

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-( () => {
+    import checkComplete from "./components/checkComplete.js";
+    import deleteIcon from "./components/deleteIcon.js";
+    
     const btn = document.querySelector("[data-form-btn]");
 
     const createTask = ((evento) => {
@@ -15,31 +17,13 @@
         titleTask.classList.add("task");
         titleTask.innerText = value;
         taskContent.appendChild(titleTask);
-        const content = `       
-            <i class="fas fa-trash-alt trashIcon icon"></i>`
         //task.innerHTML = content;
         task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
         list.appendChild(task);
 
         });
 
-
     //listener
     //Arrows funtions
     btn.addEventListener("click", createTask); 
-
-    const checkComplete = () =>{
-        const i = document.createElement("i");
-        i.classList.add('far', 'fa-check-square', 'icon');
-        i.addEventListener("click",completeTask);
-
-        return i;
-    }
-    //Immediately invoker function expression IIFE
-    const completeTask = (event) =>{
-        const element = event.target;
-        element.classList.toggle("fas");
-        element.classList.toggle("completeIcon");
-        element.classList.toggle("far");
-    }
-})();
